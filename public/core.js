25becddde1aa13a7235e85db0c7e48db3ce94ff3
tjm5114 <tjm5114@gmail.com>
2015-06-13 13:13:20 -0400

    D3 bar chart implemented for cached sensor values

--- public/core.js
+++ public/core.js
@@ -7,6 +7,24 @@ function mainController($scope, $http) {
     $http.get('/api/myvalues')
         .success(function(data) {
             $scope.myvalues = data;
+            
+            var z = 0;
+            var chartData = new Array()
+            while (z < $scope.myvalues.length) {                           
+                chartData.push(JSON.stringify($scope.myvalues[z].SensorVal)); //add sensor value to the chartData array                
+                z++;
+            }         
+           
+        var x = d3.scale.linear()
+            .domain([0, d3.max(chartData)])
+            .range([0, 420]);
+
+        d3.select(".chart")
+          .selectAll("div")
+            .data(chartData)
+          .enter().append("div")
+            .style("width", function(d) { return x(d) + "px"; })
+            .text(function(d) { return d; });
         })
         .error(function(data) {
             console.log('Error: ' + data);
