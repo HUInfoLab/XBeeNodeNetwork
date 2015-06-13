3bea3c723c443490715757acb2cc2c8c7a4dfaaa
tjm5114 <tjm5114@gmail.com>
2015-06-13 14:33:56 -0400

    Changed D3 graph from bar to Line

--- public/core.js
+++ public/core.js
@@ -6,25 +6,64 @@ function mainController($scope, $http) {
     // when landing on the page, get all values and show them
     $http.get('/api/myvalues')
         .success(function(data) {
-            $scope.myvalues = data;
+          //collect data from the database  
+          $scope.myvalues = data;
             
-            var z = 0;
-            var chartData = new Array()
-            while (z < $scope.myvalues.length) {                           
-                chartData.push(JSON.stringify($scope.myvalues[z].SensorVal)); //add sensor value to the chartData array                
+          //initiate variables for d3 visualization 
+          var z = 0;
+          var chartData = {name:'sensors', children: []};          
+          
+        //add sensor value to the chartData array 
+          while (z < $scope.myvalues.length) {                           
+                chartData.children.push({'timeStamp': z, 'value':JSON.stringify($scope.myvalues[z].SensorVal)}); //use push to place the data after the last entry
+               // console.log("The array looks like: " + JSON.stringify(chartData)); //Just some debug checking
                 z++;
-            }         
-           
-        var x = d3.scale.linear()
-            .domain([0, d3.max(chartData)])
-            .range([0, 420]);
-
-        d3.select(".chart")
-          .selectAll("div")
-            .data(chartData)
-          .enter().append("div")
-            .style("width", function(d) { return x(d) + "px"; })
-            .text(function(d) { return d; });
+            } 
+          
+          //start d3 parameter initialization 
+          var vis = d3.select("#chart"),
+          WIDTH = 1000,          
+          HEIGHT = 500,          
+          MARGINS = {
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 50
+          },  
+          xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([0,20]), // change to dynamic ranges depending on data         
+          yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([100,1000]), // same goes for the yScale         
+          xAxis = d3.svg.axis()
+          .scale(xScale),        
+          yAxis = d3.svg.axis()
+          .scale(yScale)
+          .orient("left");
+          
+          //fix xaxis to bottom
+          vis.append("svg:g")
+          .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+          .call(xAxis);
+          
+          //fix y axis to the left 
+          vis.append("svg:g")
+          .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+          .call(yAxis);
+          
+          //define function that draws the lines from data passed to it setting the x and y variables
+          var lineGen = d3.svg.line()
+          .x(function(d) {
+            return xScale(d.timeStamp); //currently just index in the database TO-DO change to a real time stamp
+          })
+          .y(function(d) {
+            return yScale(d.value);
+          })
+          .interpolate("basis");
+          
+          vis.append('svg:path')
+          .attr('d', lineGen(chartData.children)) //pass your data object into here
+          .attr('stroke', 'green') //style and color setting
+          .attr('stroke-width', 2)
+          .attr('fill', 'none');
+          
         })
         .error(function(data) {
             console.log('Error: ' + data);
